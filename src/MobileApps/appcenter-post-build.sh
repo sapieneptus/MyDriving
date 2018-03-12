ada9af59b9df8b606f3a450e1473d8010af57690
Chris Fuentes <cfuentes@g.harvard.edu>
2018-03-12 21:36:47 +0100

    update post build

--- src/MobileApps/appcenter-post-build.sh
+++ src/MobileApps/appcenter-post-build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-if [ "$MOBILECENTER_BRANCH" == "chfuen/iOS-demo" ];
+if [ "$MOBILECENTER_BRANCH" == "chfuen/demo" ];
 then
     msbuild /p:Configuration=Debug $MOBILECENTER_SOURCE_DIRECTORY/src/MobileApps/MyDriving/MyDriving.UITests/MyDriving.UITests.csproj
     appcenter test run uitest \
-                --app "GotoCPH-2017/MyDriving-for-iOS" \
-                --devices "MonkeyFest-Singapore-2017/ios-smoketest-devices" \
+                --app "chfuen/MyDriving-iOS-d-d-d-Demo" \
+                --devices "chfuen/ios-demo-set" \
                 --app-path $MOBILECENTER_OUTPUT_DIRECTORY/MyDrivingiOS.ipa \
                 --test-series "master" \
                 --locale "en_US" \
                 --build-dir $MOBILECENTER_SOURCE_DIRECTORY/src/MobileApps/MyDriving/MyDriving.UITests/bin/Debug/ \
-                --token 557934aca03ab3096c444e38ba1bac0f3f95708a
+                --token e523d067ef6057d6ac8db59cd8b621be771cfa5d
 else
     echo "Current branch is $MOBILECENTER_BRANCH"
 fi
